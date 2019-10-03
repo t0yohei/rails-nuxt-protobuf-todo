8f936a82086b98c2eecd593679575d115d378dde
t0yohei <inuneko690121@gmail.com>
2019-10-03 13:47:59 +0900

    Nuxt TypeScript を導入

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,30 +2,18 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        rails-vue-protobuf-todo-frontend
-      </h1>
-      <h2 class="subtitle">
-        My marvelous Nuxt.js project
-      </h2>
+      <h1 class="title">rails-vue-protobuf-todo-frontend</h1>
+      <h2 class="subtitle">My marvelous Nuxt.js project</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import Logo from '../components/Logo.vue'
 
 export default {
   components: {
