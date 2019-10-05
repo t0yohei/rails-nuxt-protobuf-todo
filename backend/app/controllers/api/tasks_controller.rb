82fb7d7ba37ea69b276f8c54db57bd27a430dbd7
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2019-10-05 15:53:59 +0900

    CRUDのAPIを作成

--- backend/app/controllers/api/tasks_controller.rb
+++ backend/app/controllers/api/tasks_controller.rb
@@ -1,6 +1,34 @@
 class Api::TasksController < ApplicationController
   def index
-    @tasks = Task.all
-    render json: @tasks
+    tasks = Task.all
+    render json: tasks, status: :ok
+  end
+
+  def show
+    task = Task.find(params[:id])
+    render json: task, status: :ok
+  end
+
+  def create
+    task = Task.create(task_params)
+    render json: task, status: :created
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    render json: task, status: :ok
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    render json: task, status: :no_content
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title)
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :tasks, only: [:index]
+    resources :tasks
   end
 end
