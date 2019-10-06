cf62c79b3091166866eb5f08e96f2fefec31423f
t0yohei <inuneko690121@gmail.com>
2019-10-06 22:05:10 +0900

    NewTaskFormコンポーネントを作成

--- frontend/components/tasks/Index.vue
+++ frontend/components/tasks/Index.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
     <edit-tasks-form :tasks="tasks"></edit-tasks-form>
-    <input
-      v-model="newTaskTitle"
-      placeholder="新規タスク"
-      class="newTaskTitleInput"
-    />
-    <button @click="addTask()">追加</button>
+    <new-task-form @add-task-clicked="addTask"></new-task-form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import EditTasksForm from './EditTasksForm.vue'
+import NewTaskForm from './NewTaskForm.vue'
 
 export default Vue.extend({
   components: {
-    EditTasksForm
+    EditTasksForm,
+    NewTaskForm
   },
 
   data() {
     return {
-      tasks: [{ id: 0, title: '' }],
-      newTaskTitle: ''
+      tasks: [{ id: 0, title: '' }]
     }
   },
 
@@ -35,23 +31,14 @@ export default Vue.extend({
       const tasks = await this.$axios.$get('tasks')
       this.tasks = tasks
     },
-    async addTask() {
-      if (this.newTaskTitle === '') {
-        return
-      }
+    async addTask(newTaskTitle: string) {
       await this.$axios.$post('tasks', {
-        title: this.newTaskTitle
+        title: newTaskTitle
       })
-      this.newTaskTitle = ''
       this.fetchTasks()
     }
   }
 })
 </script>
 
-<style scoped>
-.newTaskTitleInput {
-  margin: 10px;
-  padding: 10px;
-}
-</style>
+<style scoped></style>
---
+++ frontend/components/tasks/NewTaskForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <input
+      v-model="newTaskTitle"
+      placeholder="新規タスク"
+      class="new-task-title-input"
+    />
+    <button class="add-task-button" @click="addTask()">追加</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      newTaskTitle: ''
+    }
+  },
+
+  methods: {
+    addTask() {
+      if (this.newTaskTitle === '') {
+        return
+      }
+      this.$emit('add-task-clicked', this.newTaskTitle)
+      this.newTaskTitle = ''
+    }
+  }
+})
+</script>
+
+<style scoped>
+.new-task-title-input,
+.add-task-button {
+  margin: 10px;
+  padding: 10px;
+}
+</style>
