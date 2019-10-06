21ef6af574ab45c27bc152d3e3f81391965ed8fd
t0yohei <inuneko690121@gmail.com>
2019-10-06 21:20:29 +0900

    TasksIndexコンポーネントを切り出した

---
+++ frontend/pages/tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TasksIndex />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TasksIndex from '../components/tasks/Index.vue'
+
+export default Vue.extend({
+  components: {
+    TasksIndex
+  }
+})
+</script>
+
+<style scoped></style>
