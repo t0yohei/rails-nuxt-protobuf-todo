dac1cef8db77027a94d1386da1d674dedde9992b
t0yohei <inuneko690121@gmail.com>
2019-10-27 23:10:31 +0900

    proto/tasks.vue をtasksProto.vue に変更
    proto/tasks の場合 proxy の関係でnuxtのpageを呼び出したつもりが、railsを呼び出すような感じになっていたため

--- frontend/pages/tasksProto.vue
+++ frontend/pages/tasksProto.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import TasksProto from '../../components/TasksProto.vue'
+import TasksProto from '../components/TasksProto.vue'
 
 export default Vue.extend({
   components: {
