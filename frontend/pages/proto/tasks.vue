fc11b765ee37eed3745f426f6d2dc6c9a68b115a
t0yohei <inuneko690121@gmail.com>
2019-10-27 22:11:29 +0900

    自動生成したprotoファイルを元にサンプルをとりあえず作成

---
+++ frontend/pages/proto/tasks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ task }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Task } from '../../plugins/proto/task_pb'
+
+export default Vue.extend({
+  data() {
+    return {
+      task: new Task()
+    }
+  },
+
+  created() {
+    const task = new Task()
+    task.setId(1)
+    task.setTitle('title')
+    this.task = task
+  }
+})
+</script>
+
+<style scoped></style>
