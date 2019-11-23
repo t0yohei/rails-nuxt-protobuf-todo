3926a3ba910a04ba685902af29d354c74a2f4540
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2019-11-23 14:47:40 +0900

    single quote に統一

--- backend/spec/models/task_spec.rb
+++ backend/spec/models/task_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  context "validationのテスト" do
+  context 'validationのテスト' do
     context 'title' do
       subject { Task.new(title: title) }
 
-      describe "title が0文字のとき" do
+      describe 'title が0文字のとき' do
         let(:title) { '' }
         it { is_expected.to be_invalid }
       end
 
-      describe "title が1文字のとき" do
+      describe 'title が1文字のとき' do
         let(:title) { '1' }
         it { is_expected.to be_valid }
       end
 
-      describe "title が25文字のとき" do
+      describe 'title が25文字のとき' do
         let(:title) { build_random_string(25) }
         it { is_expected.to be_valid }
       end
 
-      describe "title が26文字のとき" do
+      describe 'title が26文字のとき' do
         let(:title) { build_random_string(26) }
         it { is_expected.to be_invalid }
       end
@@ -29,17 +29,17 @@ RSpec.describe Task, type: :model do
     context 'description' do
       subject { Task.new(title: 'title', description: description) }
 
-      describe "description が0文字のとき" do
+      describe 'description が0文字のとき' do
         let(:description) { '' }
         it { is_expected.to be_valid }
       end
 
-      describe "description が100文字のとき" do
+      describe 'description が100文字のとき' do
         let(:description) { build_random_string(100) }
         it { is_expected.to be_valid }
       end
 
-      describe "description が101文字のとき" do
+      describe 'description が101文字のとき' do
         let(:description) { build_random_string(101) }
         it { is_expected.to be_invalid }
       end
