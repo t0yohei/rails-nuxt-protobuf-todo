983cca8190fbcf0eb8bf9b04a5476a208af0a159
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2019-11-23 14:46:41 +0900

    テストをリファクタリング

--- backend/spec/models/task_spec.rb
+++ backend/spec/models/task_spec.rb
@@ -3,45 +3,50 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   context "validationのテスト" do
     context 'title' do
-      it "title が0文字だと無効なこと" do
-        task = Task.new(title: '')
-        expect(task).to be_invalid
+      subject { Task.new(title: title) }
+
+      describe "title が0文字のとき" do
+        let(:title) { '' }
+        it { is_expected.to be_invalid }
       end
 
-      it "title が1文字だと有効なこと" do
-        task = Task.new(title: '1')
-        expect(task).to be_valid
+      describe "title が1文字のとき" do
+        let(:title) { '1' }
+        it { is_expected.to be_valid }
       end
 
-      it "title が25文字だと有効なこと" do
-        task = Task.new(title: '1234567890123456789012345')
-        expect(task).to be_valid
+      describe "title が25文字のとき" do
+        let(:title) { build_random_string(25) }
+        it { is_expected.to be_valid }
       end
 
-      it "title が26文字だと無効なこと" do
-        task = Task.new(title: '12345678901234567890123456')
-        expect(task).to be_invalid
+      describe "title が26文字のとき" do
+        let(:title) { build_random_string(26) }
+        it { is_expected.to be_invalid }
       end
     end
 
     context 'description' do
-      let(:title) { 'title' }
-      it "description が0文字だと有効なこと" do
-        task = Task.new(title: title, description: '')
-        expect(task).to be_valid
+      subject { Task.new(title: 'title', description: description) }
+
+      describe "description が0文字のとき" do
+        let(:description) { '' }
+        it { is_expected.to be_valid }
       end
 
-      it "description が100文字だと有効なこと" do
-        description100 = (1..100).inject('') { |result| result << rand(1..9).to_s }
-        task = Task.new(title: title, description: description100)
-        expect(task).to be_valid
+      describe "description が100文字のとき" do
+        let(:description) { build_random_string(100) }
+        it { is_expected.to be_valid }
       end
 
-      it "description が101文字だと無効なこと" do
-        description101 = (1..101).inject('') { |result| result << rand(1..9).to_s }
-        task = Task.new(title: title, description: description101)
-        expect(task).to be_invalid
+      describe "description が101文字のとき" do
+        let(:description) { build_random_string(101) }
+        it { is_expected.to be_invalid }
       end
     end
+  end
+
+  def build_random_string(length)
+    (1..length).inject('') { |result| result << rand(1..9).to_s }
   end
 end
