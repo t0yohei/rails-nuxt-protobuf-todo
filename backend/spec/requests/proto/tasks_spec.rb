c2bfd6f5af5a0e9db9ee12303c1db5cc085cf111
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2019-12-03 10:44:58 +0900

    indexメソッドのテスト条件を記載

--- backend/spec/requests/proto/tasks_spec.rb
+++ backend/spec/requests/proto/tasks_spec.rb
@@ -2,9 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Proto::Tasks", type: :request do
   describe "GET /proto/tasks" do
-    it "works! (now write some real specs)" do
-      get proto_tasks_index_path
+    it "特に条件を指定しなくても、200を返すこと" do
+      get proto_tasks_path
       expect(response).to have_http_status(200)
+    end
+
+    context "taskが1件も存在しないとき" do
+      it "空の encoded_data が返却されること" do
+
+      end
+    end
+
+    context "taskが1件存在するとき" do
+
+      it "task1件分の encoded_data が返却されること" do
+
+      end
+    end
+
+    context "taskが2件存在するとき" do
+      it "task2件分の encoded_data が返却されること" do
+
+      end
     end
   end
 end
