69e552ea5d89a5090d2a1e1b4bedc3567940d0d1
Kyohei Toyoda <kyohei.toyoda@crowdworks.co.jp>
2019-12-16 15:29:04 +0900

    seedを追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: '大掃除', description: '年末なので気だるいけどな大掃除をする')
+Task.create(title: 'advent calendar を書く', description: 'そろそろ書き始めないとヤバイ')
